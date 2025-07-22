127f6a330f499d40594e1b8ae4510c4764ee303d
yoonyeoul <mahaul7486@gmail.com>
2025-07-22 16:28:59 +0900

    react-icons 최적화 설정 추가 - 아이콘 표시 문제 해결

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,14 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   trailingSlash: true,
+  experimental: {
+    optimizePackageImports: ['react-icons'],
+  },
   images: {
     domains: ['calculator.net'],
     unoptimized: true,
   },
+  transpilePackages: ['react-icons'],
 };
 
 export default nextConfig;
